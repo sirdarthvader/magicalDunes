22bbf39b1ba3d1f445945a6d117ab838ff7f3ca3
sirdarthvader <ashishcodes4@gmail.com>
2022-10-08 00:03:36 +0530

    changed class components to functional component

--- src/Components/Pages/Contact.js
+++ src/Components/Pages/Contact.js
@@ -1,82 +1,80 @@
-import React, {Component} from "react"
-import avatar from "./images/avatar.jpeg"
-import {SOCIAL_LINKS} from "../../constants/links"
+import React, { Component } from 'react';
+import avatar from './images/avatar.jpeg';
+import { SOCIAL_LINKS } from '../../constants/links';
 
-class Contact extends Component {
-  render() {
-    return (
-      <div className="contact-us">
-        <div className="jumbotron text-center">
-          <img
-            className="img-fluid"
-            style={{width: "200px", height: "auto", borderRadius: "50%"}}
-            src={avatar}
-            alt=""
-          />
-          <hr style={{color: "white"}} />
-          <h6 className="">
-            Email: <a href={SOCIAL_LINKS.mail}>ashishcodes4@gmail.com</a>
-          </h6>
-          <div className="links">
-            <div className="github">
-              <a
-                href={SOCIAL_LINKS.github}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <i className="fab fa-github-square" />
-              </a>
-            </div>
-            <div className="linkedin">
-              <a
-                href={SOCIAL_LINKS.linkedin}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <i className="fab fa-linkedin" />
-              </a>
-            </div>
-            <div className="twitter">
-              <a
-                href={SOCIAL_LINKS.twitter}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <i className="fab fa-twitter-square" />
-              </a>
-            </div>
-            <div className="medium">
-              <a
-                href={SOCIAL_LINKS.medium}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <i className="fab fa-medium" />
-              </a>
-            </div>
-            <div className="freecodecamp">
-              <a
-                href={SOCIAL_LINKS.freecodecamp}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <i className="fab fa-free-code-camp" />
-              </a>
-            </div>
-            <div className="stackoverflow">
-              <a
-                href={SOCIAL_LINKS.stackoverflow}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <i className="fab fa-stack-overflow"></i>
-              </a>
-            </div>
+const Contact = () => {
+  return (
+    <div className="contact-us">
+      <div className="jumbotron text-center">
+        <img
+          className="img-fluid"
+          style={{ width: '200px', height: 'auto', borderRadius: '50%' }}
+          src={avatar}
+          alt=""
+        />
+        <hr style={{ color: 'white' }} />
+        <h6 className="">
+          Email: <a href={SOCIAL_LINKS.mail}>ashishcodes4@gmail.com</a>
+        </h6>
+        <div className="links">
+          <div className="github">
+            <a
+              href={SOCIAL_LINKS.github}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fab fa-github-square" />
+            </a>
+          </div>
+          <div className="linkedin">
+            <a
+              href={SOCIAL_LINKS.linkedin}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fab fa-linkedin" />
+            </a>
+          </div>
+          <div className="twitter">
+            <a
+              href={SOCIAL_LINKS.twitter}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fab fa-twitter-square" />
+            </a>
+          </div>
+          <div className="medium">
+            <a
+              href={SOCIAL_LINKS.medium}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fab fa-medium" />
+            </a>
+          </div>
+          <div className="freecodecamp">
+            <a
+              href={SOCIAL_LINKS.freecodecamp}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fab fa-free-code-camp" />
+            </a>
+          </div>
+          <div className="stackoverflow">
+            <a
+              href={SOCIAL_LINKS.stackoverflow}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fab fa-stack-overflow"></i>
+            </a>
           </div>
         </div>
       </div>
-    )
-  }
-}
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
--- src/Components/Pages/Skills.js
+++ src/Components/Pages/Skills.js
@@ -1,57 +1,54 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-class Skills extends Component {
-  render() {
-    return (
-      <div className="skills">
-        <div className="list">
-          <div className='skill'>
-           <i className="devicon-javascript-plain colored" />
-          </div>
-          <div className='skill'>
-            <i className="devicon-nodejs-plain-wordmark colored" />
-          </div>
-          <div className='skill'>
+const Skills = () => {
+  return (
+    <div className="skills">
+      <div className="list">
+        <div className="skill">
+          <i className="devicon-javascript-plain colored" />
+        </div>
+        <div className="skill">
+          <i className="devicon-nodejs-plain-wordmark colored" />
+        </div>
+        <div className="skill">
           <i className="devicon-express-original-wordmark" />
-
-          </div>
-          <div className='skill'>
-            <i className="devicon-mongodb-plain-wordmark colored" />
-          </div>
-          <div className='skill'>
-            <i className="devicon-babel-plain colored" />
-          </div>
-          <div className='skill'>
-            <i className="devicon-webpack-plain-wordmark colored" />
-          </div>
-          <div className='skill'>
-            <i className="devicon-html5-plain-wordmark colored" />
-          </div>
-          <div className='skill'>
-            <i className="devicon-css3-plain-wordmark colored" />
-          </div>
-          <div className='skill'>
-            <i className="devicon-git-plain-wordmark colored" />
-          </div>
-          <div className='skill'>
-            <i className="devicon-github-plain colored" />
-          </div>
-          <div className='skill'>
-            <i className="devicon-jquery-plain-wordmark colored" />
-          </div>
-          <div className='skill'>
-            <i className="devicon-visualstudio-plain-wordmark colored" />
-          </div>
-          <div className='skill'>
-            <i className="devicon-typescript-plain colored" />
-          </div>
-          <div className='skill'>
-            <i className="devicon-d3js-plain" />
-          </div>
+        </div>
+        <div className="skill">
+          <i className="devicon-mongodb-plain-wordmark colored" />
+        </div>
+        <div className="skill">
+          <i className="devicon-babel-plain colored" />
+        </div>
+        <div className="skill">
+          <i className="devicon-webpack-plain-wordmark colored" />
+        </div>
+        <div className="skill">
+          <i className="devicon-html5-plain-wordmark colored" />
+        </div>
+        <div className="skill">
+          <i className="devicon-css3-plain-wordmark colored" />
+        </div>
+        <div className="skill">
+          <i className="devicon-git-plain-wordmark colored" />
+        </div>
+        <div className="skill">
+          <i className="devicon-github-plain colored" />
+        </div>
+        <div className="skill">
+          <i className="devicon-jquery-plain-wordmark colored" />
+        </div>
+        <div className="skill">
+          <i className="devicon-visualstudio-plain-wordmark colored" />
+        </div>
+        <div className="skill">
+          <i className="devicon-typescript-plain colored" />
+        </div>
+        <div className="skill">
+          <i className="devicon-d3js-plain" />
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Skills;
